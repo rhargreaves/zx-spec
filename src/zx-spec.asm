6249100f602799b22a2017cc1936e4f219c74554
rhargreaves <robert@roberthargreaves.net>
2017-10-09 19:28:47 +0100

    Refactor

--- src/zx-spec.asm
+++ src/zx-spec.asm
@@ -1,18 +1,18 @@
 ; Constants
 prog_start	equ	8000h
-stream_scr	equ	2		; Screen stream
+stream_scr	equ	2		; screen stream
 
 ; ROM Routines
-cl_all		equ	0dafh
-chan_open	equ	1601h
-pr_string	equ	203ch
+cl_all		equ	0dafh		; clear screen
+chan_open	equ	1601h		; open channel
+pr_string	equ	203ch		; print string
 
 ; Start
 		org	prog_start
 
 proc
 init		call	cl_all		; clear screen
-		ld	a,stream_scr	; Upper screen
+		ld	a,stream_scr	; upper screen
 		call	chan_open	; open channel
 		ld	de,banner_txt	; text address
 		ld	bc,banner_txt_end-banner_txt	; string length
@@ -20,7 +20,9 @@ init		call	cl_all		; clear screen
 endp
 		ret
 
+; Resources
 banner_txt	defb	'ZX Spec - The TDD Framework'
 banner_txt_end	equ	$
 
+; End
 		end	prog_start
