abce02dc1a30b869300eee36b29d5e61320d64c5
rhargreaves <robert@roberthargreaves.net>
2017-10-12 18:10:00 +0100

    Add timeout for printout production

--- test.py
+++ test.py
@@ -6,35 +6,46 @@ import glob
 import unittest
 
 class ZxSpecTestCase(unittest.TestCase):
-    def setUp(self):
-        clean()
 
-    def tearDown(self):
+    @classmethod
+    def setUpClass(self):
         clean()
+        self.output = run_zx_spec()
 
     def test_zx_spec_header_displayed(self):
-        ZX_SPEC_OUTPUT_FILE = "printout.txt"
+        self.assertRegexpMatches(self.output, 'ZX Spec - The TDD Framework')
 
-        proc = subprocess.Popen([
-            "fuse",
-            "--tape", "bin/zx-spec-test.tap",
-            "--auto-load",
-            "--no-autosave-settings"])
+    def test_all_tests_pass(self):
+        self.assertRegexpMatches(self.output, 'Pass: 0, Fail: 0')
 
-        while not os.path.exists(ZX_SPEC_OUTPUT_FILE):
-            time.sleep(0.1)
-
-        time.sleep(1)
-        proc.kill()
-
-        with open(ZX_SPEC_OUTPUT_FILE, 'r') as f:
-            zx_spec_output = f.read()
-
-            self.assertRegexpMatches(zx_spec_output, 'ZX Spec - The TDD Framework')
+    @classmethod
+    def tearDownClass(self):
+        clean()
 
 def clean():
     for f in glob.glob("printout.*"):
         os.remove(f)
+
+def run_zx_spec():
+    ZX_SPEC_OUTPUT_FILE = "printout.txt"
+    proc = subprocess.Popen([
+        "fuse",
+        "--tape", "bin/zx-spec-test.tap",
+        "--auto-load",
+        "--no-autosave-settings"])
+
+    wait_count = 0
+    while not os.path.exists(ZX_SPEC_OUTPUT_FILE):
+        time.sleep(0.1)
+        wait_count += 1
+        if wait_count == 20:
+            raise 'Output file not produced in time'
+
+    time.sleep(1)
+    proc.kill()
+
+    with open(ZX_SPEC_OUTPUT_FILE, 'r') as f:
+        return f.read()
 
 if __name__ == '__main__':
     unittest.main()
