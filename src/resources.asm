bf18211112d5d41fc4ce6607cc9c2c342fa8578e
rhargreaves <robert@roberthargreaves.net>
2017-10-14 15:51:41 +0100

    Make pretty

--- src/resources.asm
+++ src/resources.asm
@@ -1,5 +1,5 @@
 ; Resources
-banner_txt		db	'ZX Spec - The TDD Framework', nl, nl
+banner_txt		db	'   ZX Spec: The TDD Framework', nl, nl
 banner_txt_end		equ	$
 ok_txt			db	nl, 'Pass: '
 ok_txt_end		equ	$
@@ -7,7 +7,7 @@ fail_txt		db	', Fail: '
 fail_txt_end		equ	$
 total_txt		db	', Total: '
 total_txt_end		equ	$
-pause_txt		db	nl, nl, 'Press any key to continue'
+pause_txt		db	nl, nl, 'Press any key.'
 pause_txt_end		equ	$
 expected_txt		db	'Expected: ' 
 expected_txt_end	equ	$
