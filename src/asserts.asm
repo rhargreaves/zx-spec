1c1a92546edec8a1b45de57b9f0020fbf4a7387e
rhargreaves <robert@roberthargreaves.net>
2017-10-14 16:59:40 +0100

    Move assert routines closer to macros

--- src/asserts.asm
+++ src/asserts.asm
@@ -7,6 +7,41 @@ assert_fail		macro
 			call	assert_fail_r
 			endm
 
+assert_pass_r		proc
+			ld		hl,num_pass
+			inc		(hl)
+			print_char	period
+			ret
+			endp
+			
+assert_fail_r		proc
+local			print_group_end
+			set_border_colour	red_border	; Set border to red
+			ld	hl,num_fail
+			inc	(hl)
+			print_char	cross
+			ld	hl,shown_names
+			bit	0,(hl)				; Group name shown already?
+			jp	nz,(print_group_end)
+			ld	de,(cur_group_name_addr)	; text address
+			ld	bc,(cur_group_name_len)		; string length
+			ld	a,c
+			cp	0				; is group name undefined?
+			jp	z,(print_group_end)		; skip printing of name if so
+			print_newline
+			call	pr_string
+			ld	hl,shown_names
+			set	0,(hl)				; Set shown group name
+print_group_end		print_newline
+			print_char space			; indent test name
+			ld	de,(cur_test_name_addr)		; text address
+			ld	bc,(cur_test_name_len)		; string length
+			call	pr_string			; print string
+			print_newline
+			print_newline
+			ret
+			endp
+
 assert_a_equals		macro	val			
 local 			passes, done
 			push	af		; store copy of A as it gets overwitten
--- src/zx-spec.asm
+++ src/zx-spec.asm
@@ -1,8 +1,8 @@
+			jp	init
 include src/constants.asm
 include src/rom.asm
 include src/macros.asm
 include src/asserts.asm
-			jp	init
 include src/state.asm
 include src/resources.asm
 include src/routines.asm
