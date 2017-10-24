9ed7d0d10b344bc393a6ad004c7b71106f6039a3
rhargreaves <robert@roberthargreaves.net>
2017-10-24 20:29:56 +0100

    Re-order routines into more logical structure

--- src/includes/asserts.asm
+++ src/includes/asserts.asm
@@ -1,4 +1,8 @@
 
+;-------------------------
+; Public
+;-------------------------
+
 assert_pass		macro
 			call	assert_pass_r
 			endm
@@ -7,151 +11,79 @@ assert_fail		macro
 			call	assert_fail_r
 			endm
 
-inc_done		macro		num_done, done_txt_start, done_txt_end
-			push		hl
-			ld		hl,num_done
-			inc		(hl)				; Increment number done
-			print_text	done_txt_start, done_txt_end	; Print 'done' indicator
-			pop		hl
+assert_a_equal		macro	val
+			push	bc		
+			ld	c,val		; Store expected in C
+			call	assert_a_equal_r
+			pop	bc
+			endm			
+
+assert_b_equal		macro	val			
+			assert_reg_equal val, b
 			endm
 
-assert_pass_r		proc
-			inc_done	num_pass, pass_indicator_txt, pass_indicator_txt_end	; Increment number passed
-			ret
-			endp
-			
-assert_fail_r		proc
-			local			print_group_end
-			set_border_colour	red_border		; Set border to red
-			inc_done	num_fail, fail_indicator_txt, fail_indicator_txt_end	; Increment number failed
-			fail_ink
-			ld		hl,shown_names
-			bit		0,(hl)				; Group name shown already?
-			jp		nz,print_group_end		; Skip if so.
-			push		de
-			ld		de,(cur_group_name_len)		; get string length
-			inc		e				; +1 -1 = 0
-			dec		e				; sets Z if group name undefined
-			pop		de
-			jp		z,print_group_end		; skip printing of name if so
-			print_newline
-			print_text_with_len	(cur_group_name_addr), (cur_group_name_len)
-			ld		hl,shown_names
-			set		0,(hl)				; Set shown group name
-print_group_end		print_newline
-			print_char space				; indent test name
-			print_text_with_len	(cur_test_name_addr), (cur_test_name_len)
-			print_newline
-			print_newline
-			normal_ink
-			ret
-			endp
-
-assert_cond		macro	val, cond
-			local	passes, done
-			jp	cond,passes	; pass if flag set
-			assert_fail		; otherwise, fail
-			jr	done
-passes			assert_pass
-done			equ	$
+assert_c_equal		macro	val			
+			assert_reg_equal val, c
 			endm
 
-assert_z_set		macro	val
-			assert_cond	val, z
+assert_d_equal		macro	val			
+			assert_reg_equal val, d
 			endm
 
-assert_z_reset		macro	val
-			assert_cond	val, nz
+assert_e_equal		macro	val			
+			assert_reg_equal val, e
 			endm
 
-assert_carry_set	macro	val
-			assert_cond	val, c
-			endm
-
-assert_carry_reset	macro	val
-			assert_cond	val, nc
-			endm
-
-assert_s_set		macro	val
-			assert_cond	val, m
-			endm
-
-assert_s_reset		macro	val
-			assert_cond	val, p
-			endm
-
-assert_p_v_set		macro	val
-			assert_cond	val, pe
-			endm
-
-assert_p_v_reset	macro	val
-			assert_cond	val, po
-			endm							
-
-assert_a_equal_r	proc			; C = expected, A = actual
-			local	passes, done
-			cp	c		; does A = expected?
-			jp	z,passes	; pass if so
-			assert_fail		; otherwise, fail
-			fail_ink
-			print_text expected_txt, expected_txt_end
-			call	print_num_in_c
-			print_text actual_txt, actual_txt_end
-			ld	c,a
-			call	print_num_in_c
-			print_newline
-			print_newline
-			normal_ink
-			jr	done
-passes			assert_pass
-done			ret
-			endp
-
-assert_a_not_equal_r	proc			; C = not expected, A = actual
-			local	passes, done
-			cp	c		; does A = not expected?
-			jr	nz,passes	; pass if it doesn't
-			assert_fail		; otherwise, fail
-			jr	done
-passes			assert_pass
-done			ret
-			endp
-
-assert_hl_equal_r	proc			; HL = actual, BC = expected
-			local	passes, done
-			push	hl
-			push	hl
-			sbc	hl,bc		; Subtract val from HL
+assert_h_equal		macro	val
+			push	hl	
+			assert_reg_equal val, h
 			pop	hl
-			jp	z,passes	; pass if same
-			push	hl
-			assert_fail		; otherwise, fail
-			fail_ink
-			print_text expected_txt, expected_txt_end
-			call	print_num_in_bc
-			print_text actual_txt, actual_txt_end
-			pop	bc		; pop HL into BC
-			call	print_num_in_bc
-			print_newline
-			print_newline
-			normal_ink
-			jp	done
-passes			assert_pass
-done			pop	hl
-			ret
-			endp
+			endm
 
-assert_hl_not_equal_r	proc			; HL = actual, BC = unexpected
-			local	passes, done
-			push	hl
-			sbc	hl,bc		; Subtract val from HL
-			jp	nz,passes	; pass if different
-			assert_fail		; otherwise, fail
-			jr	done
-passes			assert_pass
-done			pop	hl
-			ret
-			endp			
+assert_l_equal		macro	val	
+			push	hl		
+			assert_reg_equal val, l
+			pop	hl
+			endm
+
+assert_a_not_equal	macro	val
+			push	bc
+			ld	c,val
+			call	assert_a_not_equal_r
+			pop	bc
+			endm
+
+assert_b_not_equal	macro	val
+			assert_reg_not_equal	val, b
+			endm
+
+assert_c_not_equal	macro	val
+			assert_reg_not_equal	val, c
+			endm
+
+assert_d_not_equal	macro	val
+			assert_reg_not_equal	val, d
+			endm
+
+assert_e_not_equal	macro	val
+			assert_reg_not_equal	val, e
+			endm
+
+assert_h_not_equal	macro	val
+			assert_reg_not_equal	val, h
+			endm
+
+assert_l_not_equal	macro	val
+			assert_reg_not_equal	val, l
+			endm
+
+assert_a_is_zero	macro
+			assert_a_equal 0
+			endm
+
+assert_a_is_not_zero	macro
+			assert_a_not_equal 0
+			endm			
 
 assert_hl_equal		macro	val			
 			push	bc
@@ -184,6 +116,69 @@ assert_ix_equal		macro	val
 			pop	de
 			endm
 
+assert_hl_not_equal	macro	val			
+			push	bc
+			ld	bc,val
+			call	assert_hl_not_equal_r
+			pop	bc
+			endm	
+
+assert_bc_not_equal	macro	val			
+			push	hl
+			ld	h,b
+			ld	l,c
+			assert_hl_not_equal	val
+			pop	hl
+			endm
+
+assert_de_not_equal	macro	val
+			push	hl
+			ld	h,d
+			ld	l,e
+			assert_hl_not_equal	val
+			pop	hl
+			endm
+
+assert_ix_not_equal	macro	val		
+			push	de
+			ld	d,ixh
+			ld	e,ixl
+			assert_de_not_equal	val
+			pop	de
+			endm					
+
+assert_z_set		macro	val
+			assert_cond	val, z
+			endm
+
+assert_z_reset		macro	val
+			assert_cond	val, nz
+			endm
+
+assert_carry_set	macro	val
+			assert_cond	val, c
+			endm
+
+assert_carry_reset	macro	val
+			assert_cond	val, nc
+			endm
+
+assert_s_set		macro	val
+			assert_cond	val, m
+			endm
+
+assert_s_reset		macro	val
+			assert_cond	val, p
+			endm
+
+assert_p_v_set		macro	val
+			assert_cond	val, pe
+			endm
+
+assert_p_v_reset	macro	val
+			assert_cond	val, po
+			endm		
+
 assert_byte_equal	macro	mem_addr, val
 			push	af
 			ld	a,(mem_addr)
@@ -211,35 +206,6 @@ assert_word_not_equal	macro	mem_addr, val
 			assert_hl_not_equal	val
 			pop	hl
 			endm
-
-comp_str		proc	; Compares two strings
-				; Inputs: B = string length, HL = actual start, DE = expected start 
-				; Outputs: Z flag: 1 = string equal, 0 = not equal
-			local	loop, done
-loop			ld	c,(hl)			; C = Actual char
-			res	7,c			; Remove any string termination bit
-			ld	a,(de)			; A = Expected char
-			cp	c			; Compare actual with expected char
-			jp	nz,done			; Not equal.
-			inc	hl			; Next actual char
-			inc	de			; Next expected char
-			djnz	loop			; Decrement string length, loop if <> 0
-done			ret
-			endp
-
-comp_bytes		proc	; Compares two sequence of bytes
-				; Inputs: B = length, HL = actual start, DE = expected start 
-				; Outputs: Z flag: 1 = string equal, 0 = not equal
-			local	loop, done
-loop			ld	c,(hl)			; C = Actual byte
-			ld	a,(de)			; A = Expected byte
-			cp	c			; Compare actual with expected byte
-			jp	nz,done			; Not equal.
-			inc	hl			; Next actual byte
-			inc	de			; Next expected byte
-			djnz	loop			; Decrement length, loop if <> 0
-done			ret
-			endp			
 
 assert_bytes_equal	macro	bytes_1_start, bytes_1_length, bytes_2_start
 			local	fail, done
@@ -316,14 +282,154 @@ val_end			ld	b,val_end-val_start	; B = string length
 			jr	done
 pass			assert_pass
 done			equ	$
+			endm
+
+;-------------------------
+; Private
+;-------------------------
+
+inc_done		macro		num_done, done_txt_start, done_txt_end
+			push		hl
+			ld		hl,num_done
+			inc		(hl)				; Increment number done
+			print_text	done_txt_start, done_txt_end	; Print 'done' indicator
+			pop		hl
+			endm
+
+assert_pass_r		proc
+			inc_done	num_pass, pass_indicator_txt, pass_indicator_txt_end	; Increment number passed
+			ret
+			endp
+			
+assert_fail_r		proc
+			local			print_group_end
+			set_border_colour	red_border		; Set border to red
+			inc_done	num_fail, fail_indicator_txt, fail_indicator_txt_end	; Increment number failed
+			fail_ink
+			ld		hl,shown_names
+			bit		0,(hl)				; Group name shown already?
+			jp		nz,print_group_end		; Skip if so.
+			push		de
+			ld		de,(cur_group_name_len)		; get string length
+			inc		e				; +1 -1 = 0
+			dec		e				; sets Z if group name undefined
+			pop		de
+			jp		z,print_group_end		; skip printing of name if so
+			print_newline
+			print_text_with_len	(cur_group_name_addr), (cur_group_name_len)
+			ld		hl,shown_names
+			set		0,(hl)				; Set shown group name
+print_group_end		print_newline
+			print_char space				; indent test name
+			print_text_with_len	(cur_test_name_addr), (cur_test_name_len)
+			print_newline
+			print_newline
+			normal_ink
+			ret
+			endp
+
+assert_cond		macro	val, cond
+			local	passes, done
+			jp	cond,passes	; pass if flag set
+			assert_fail		; otherwise, fail
+			jr	done
+passes			assert_pass
+done			equ	$
 			endm					
 
-assert_a_equal		macro	val
-			push	bc		
-			ld	c,val		; Store expected in C
-			call	assert_a_equal_r
-			pop	bc
-			endm
+assert_a_equal_r	proc			; C = expected, A = actual
+			local	passes, done
+			cp	c		; does A = expected?
+			jp	z,passes	; pass if so
+			assert_fail		; otherwise, fail
+			fail_ink
+			print_text expected_txt, expected_txt_end
+			call	print_num_in_c
+			print_text actual_txt, actual_txt_end
+			ld	c,a
+			call	print_num_in_c
+			print_newline
+			print_newline
+			normal_ink
+			jr	done
+passes			assert_pass
+done			ret
+			endp
+
+assert_a_not_equal_r	proc			; C = not expected, A = actual
+			local	passes, done
+			cp	c		; does A = not expected?
+			jr	nz,passes	; pass if it doesn't
+			assert_fail		; otherwise, fail
+			jr	done
+passes			assert_pass
+done			ret
+			endp
+
+assert_hl_equal_r	proc			; HL = actual, BC = expected
+			local	passes, done
+			push	hl
+			push	hl
+			sbc	hl,bc		; Subtract val from HL
+			pop	hl
+			jp	z,passes	; pass if same
+			push	hl
+			assert_fail		; otherwise, fail
+			fail_ink
+			print_text expected_txt, expected_txt_end
+			call	print_num_in_bc
+			print_text actual_txt, actual_txt_end
+			pop	bc		; pop HL into BC
+			call	print_num_in_bc
+			print_newline
+			print_newline
+			normal_ink
+			jp	done
+passes			assert_pass
+done			pop	hl
+			ret
+			endp
+
+assert_hl_not_equal_r	proc			; HL = actual, BC = unexpected
+			local	passes, done
+			push	hl
+			sbc	hl,bc		; Subtract val from HL
+			jp	nz,passes	; pass if different
+			assert_fail		; otherwise, fail
+			jr	done
+passes			assert_pass
+done			pop	hl
+			ret
+			endp			
+
+comp_str		proc	; Compares two strings
+				; Inputs: B = string length, HL = actual start, DE = expected start 
+				; Outputs: Z flag: 1 = string equal, 0 = not equal
+			local	loop, done
+loop			ld	c,(hl)			; C = Actual char
+			res	7,c			; Remove any string termination bit
+			ld	a,(de)			; A = Expected char
+			cp	c			; Compare actual with expected char
+			jp	nz,done			; Not equal.
+			inc	hl			; Next actual char
+			inc	de			; Next expected char
+			djnz	loop			; Decrement string length, loop if <> 0
+done			ret
+			endp
+
+comp_bytes		proc	; Compares two sequence of bytes
+				; Inputs: B = length, HL = actual start, DE = expected start 
+				; Outputs: Z flag: 1 = string equal, 0 = not equal
+			local	loop, done
+loop			ld	c,(hl)			; C = Actual byte
+			ld	a,(de)			; A = Expected byte
+			cp	c			; Compare actual with expected byte
+			jp	nz,done			; Not equal.
+			inc	hl			; Next actual byte
+			inc	de			; Next expected byte
+			djnz	loop			; Decrement length, loop if <> 0
+done			ret
+			endp			
 
 assert_reg_equal	macro	val, reg			
 			push	af		; Backup A
@@ -332,107 +438,9 @@ assert_reg_equal	macro	val, reg
 			pop	af		; Restore A
 			endm
 
-assert_b_equal		macro	val			
-			assert_reg_equal val, b
-			endm
-
-assert_c_equal		macro	val			
-			assert_reg_equal val, c
-			endm
-
-assert_d_equal		macro	val			
-			assert_reg_equal val, d
-			endm
-
-assert_e_equal		macro	val			
-			assert_reg_equal val, e
-			endm
-
-assert_h_equal		macro	val
-			push	hl	
-			assert_reg_equal val, h
-			pop	hl
-			endm
-
-assert_l_equal		macro	val	
-			push	hl		
-			assert_reg_equal val, l
-			pop	hl
-			endm
-
-assert_a_not_equal	macro	val
-			push	bc
-			ld	c,val
-			call	assert_a_not_equal_r
-			pop	bc
-			endm
-
 assert_reg_not_equal	macro	val, reg
 			push	af		; Backup A
 			ld	a,reg		; Copy register into A
 			assert_a_not_equal	val
 			pop	af		; Restore A
-			endm
-
-assert_b_not_equal	macro	val
-			assert_reg_not_equal	val, b
-			endm
-
-assert_c_not_equal	macro	val
-			assert_reg_not_equal	val, c
-			endm
-
-assert_d_not_equal	macro	val
-			assert_reg_not_equal	val, d
-			endm
-
-assert_e_not_equal	macro	val
-			assert_reg_not_equal	val, e
-			endm
-
-assert_h_not_equal	macro	val
-			assert_reg_not_equal	val, h
-			endm
-
-assert_l_not_equal	macro	val
-			assert_reg_not_equal	val, l
-			endm
-
-assert_hl_not_equal	macro	val			
-			push	bc
-			ld	bc,val
-			call	assert_hl_not_equal_r
-			pop	bc
-			endm	
-
-assert_bc_not_equal	macro	val			
-			push	hl
-			ld	h,b
-			ld	l,c
-			assert_hl_not_equal	val
-			pop	hl
-			endm
-
-assert_de_not_equal	macro	val
-			push	hl
-			ld	h,d
-			ld	l,e
-			assert_hl_not_equal	val
-			pop	hl
-			endm
-
-assert_ix_not_equal	macro	val		
-			push	de
-			ld	d,ixh
-			ld	e,ixl
-			assert_de_not_equal	val
-			pop	de
-			endm
-
-assert_a_is_zero	macro
-			assert_a_equal 0
-			endm
-
-assert_a_is_not_zero	macro
-			assert_a_not_equal 0
 			endm
--- src/includes/structure.asm
+++ src/includes/structure.asm
@@ -1,4 +1,15 @@
-; Macros
+spec_init		macro
+			ld	a,normal_paper_colour	; Set border to background colour to avoid last
+							; two lines being wrong colour
+			call	border_int
+			ld 	a,normal_ink_colour	; Set ink colour
+			ld	(attr_p),a
+			call	cl_all		; clear screen
+			ld	a,output_stream	; upper screen
+			call	chan_open	; open channel
+			print_text	banner_txt, banner_txt_end
+			endm
+
 describe		macro	group_name
 			local	group_name_start, group_name_end
 			ld	hl,shown_names
@@ -19,18 +30,6 @@ test_name_end		ld	hl,test_name_start
 			ld	(cur_test_name_addr),hl
 			ld	hl,test_name_end - test_name_start
 			ld	(cur_test_name_len),hl
-			endm
-
-spec_init		macro
-			ld	a,normal_paper_colour	; Set border to background colour to avoid last
-							; two lines being wrong colour
-			call	border_int
-			ld 	a,normal_ink_colour	; Set ink colour
-			ld	(attr_p),a
-			call	cl_all		; clear screen
-			ld	a,output_stream	; upper screen
-			call	chan_open	; open channel
-			print_text	banner_txt, banner_txt_end
 			endm
 
 spec_end		macro
