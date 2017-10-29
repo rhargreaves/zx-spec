f101cf8c74ccb751c1039117d7342f93c512a5d1
rhargreaves <robert@roberthargreaves.net>
2017-10-29 22:07:26 +0000

    Refactor

--- examples/checkout-kata.asm
+++ examples/checkout-kata.asm
@@ -14,8 +14,8 @@ include src/zx-spec.asm
 
 			spec_init
 
-clear_a			macro
-			ld	a,$FF		; Set A to something other than a valid price
+clear_b			macro
+			ld	b,$FF		; Set B to something other than a valid price
 			endm			
 
 load_items		macro	items		; Loads item string.
@@ -30,37 +30,37 @@ _end			equ	$
 
 	describe 'price'
 		it 'Returns 0 for no items'
-			clear_a
+			clear_b
 			ld	hl,0		; Any mem address
 			ld	de,0		; Zero length items
 
 			call	price
 
-			assert_a_equal	0
+			assert_b_equal	0
 
 		it 'Returns price for item A'
-			clear_a
+			clear_b
 			load_items	'A'
 
 			call	price
 
-			assert_a_equal	50
+			assert_b_equal	50
 
 		it 'Returns price for item B'
-			clear_a
+			clear_b
 			load_items	'B'
 
 			call	price
 
-			assert_a_equal	30
+			assert_b_equal	30
 
 		it 'Returns price for item C'
-			clear_a
+			clear_b
 			load_items	'C'
 
 			call	price
 
-			assert_a_equal	20
+			assert_b_equal	20
 
 			spec_end
 
@@ -70,18 +70,15 @@ price			proc	; The price routine
 				; Output: A = total price
 			ld	a,(hl)		; Load first char
 			cp	'A'		; Is A?
-			jr	z,ret_A		; Return price for A
+			ld	b,50		; Load up return value
+			ret	z		; Return if A.
 			cp	'B'		; Otherwise, is B?
-			jr	z,ret_B		; Return price for B
+			ld	b,30		; Load up return value
+			ret	z		; Return if B.
 			cp	'C'		; Otherwise, is C?
-			jr	z,ret_C		; Return price for C
-			ld	a,0		; Otherwise, return 0
-			ret
-ret_A			ld	a,50
-			ret
-ret_B			ld	a,30
-			ret
-ret_C			ld	a,20
+			ld	b,20		; Load up return value
+			ret	z		; Return if C
+			ld	b,0		; Otherwise, return 0
 			ret
 			endp
 
