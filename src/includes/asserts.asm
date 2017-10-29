e4c0ca1d3407bdf6125dff0ff4d4cbe710362e74
rhargreaves <robert@roberthargreaves.net>
2017-10-29 18:54:59 +0000

    Refactor test description print code

--- src/includes/asserts.asm
+++ src/includes/asserts.asm
@@ -300,34 +300,24 @@ _assert_pass_r		proc
 			_inc_done	_zxspec_num_pass, _zxspec_text_pass_mark, _zxspec_text_pass_mark_end	; Increment number passed
 			if defined zxspec_config_verbose_output
 				_pass_ink
-				ld		hl,_zxspec_shown_names
-				bit		0,(hl)				; Group name shown already?
-				jp		nz,print_group_end		; Skip if so.
-				push		de
-				ld		de,(_zxspec_group_name_length)		; get string length
-				inc		e				; +1 -1 = 0
-				dec		e				; sets Z if group name undefined
-				pop		de
-				jp		z,print_group_end		; skip printing of name if so
-				_print_newline
-				_print_text_with_len	(_zxspec_group_name), (_zxspec_group_name_length)
-				ld		hl,_zxspec_shown_names
-				set		0,(hl)				; Set shown group name
-print_group_end			_print_newline
-				_print_char _space				; indent test name
-				_print_text_with_len	(_zxspec_test_name), (_zxspec_test_name_length)
-				_print_newline
-				_print_newline
+				call	_print_description
 				_normal_ink
 			endif
 			ret
-			endp
+			endp			
 			
 _assert_fail_r		proc
 			local		print_group_end
 			_paint_border	_zxspec_red_border
 			_inc_done	_zxspec_num_fail, _zxspec_text_fail_mark, _zxspec_text_fail_mark_end	; Increment number failed
 			_fail_ink
+			call		_print_description	
+			_normal_ink
+			ret
+			endp
+
+_print_description	proc		; Prints test description
+			local		print_group_end
 			ld		hl,_zxspec_shown_names
 			bit		0,(hl)				; Group name shown already?
 			jp		nz,print_group_end		; Skip if so.
@@ -346,9 +336,8 @@ print_group_end		_print_newline
 			_print_text_with_len	(_zxspec_test_name), (_zxspec_test_name_length)
 			_print_newline
 			_print_newline
-			_normal_ink
 			ret
-			endp
+			endp			
 
 _assert_cond		macro	val, cond
 			local	passes, done
