92c418c26e0e44f9ef34b2b10868171f599a2676
rhargreaves <robert@roberthargreaves.net>
2017-10-29 19:43:27 +0000

    Failing test for item A

--- examples/checkout-kata.asm
+++ examples/checkout-kata.asm
@@ -1,5 +1,13 @@
 ; Based on http://codekata.com/kata/kata09-back-to-the-checkout/
 
+;   Item    Unit     Special
+;           Price    Price
+;   --------------------------
+;     A     50       3 for 130
+;     B     30       2 for 45
+;     C     20
+;     D     15
+
 			org	8000h
 
 include src/zx-spec.asm
@@ -9,14 +17,38 @@ include src/zx-spec.asm
 	describe 'price'
 		it 'Returns 0 for no items'
 
-			ld	a,$FF
+			ld	a,$FF		; Set A to something other than correct result
+			ld	de,0		; Zero length items
+			ld	hl,0		; Any mem address
+
 			call	price
+
 			assert_a_equal	0
 
-			spec_end
+		it 'Returns price for item A'
 
-price			proc
-			ld	a,0
+			proc
+			local	items, items_end
+			ld	a,$FF			; Set A to something other than correct result
+			ld	hl,(items)		; Items string start address
+			ld	de,items_end-items	; Items length (1)
+
+			call	price
+
+			assert_a_equal	50
+
+			jp	items_end
+items			db	'A'
+items_end		equ	$
+			endp
+
+			spec_end			
+
+price			proc	; The price routine
+				; -----------------
+				; Input: HL = items start address, DE = items length
+				; Output: A = total price
+			ld	a,0	; Set price to 0
 			ret
 			endp
 
